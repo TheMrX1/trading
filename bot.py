a2cb20777553cadc70897348bc5c4e6d44f04fe9
XtentX <96618039+TheMrX1@users.noreply.github.com>
2025-09-29 18:41:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN not found in .env file")
 
-TRUSTED_USERS = [1085064193, 1563262750]
+TRUSTED_USERS = [1085064193, 1563262750, 829213580, 1221434895, 1229198783, 1647115336]
 
 # Хранилище активов, состояний и настроек
 user_assets = {}
